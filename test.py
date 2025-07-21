f05f08fcd29c30bfdf37a71636d16cf79fde607c
Roo <dunnillt@gmail.com>
2025-07-21 13:48:22 +0100

    created seperate training data from the testing data

--- test.py
+++ test.py
@@ -4,13 +4,16 @@ from sklearn.preprocessing import MinMaxScaler
 
 data = []
 data = np.loadtxt("diabetes_hackathon.csv", delimiter=",", dtype=str)
+data = data[1:, :]
 remove_cols = [2,3,5,6,7,8,9]
 stripped_data = np.delete(data, remove_cols, axis=1)
-stripped_data = stripped_data[1:, :]
 
+data_size = len(data)
 
-def normalise(xi, xmin, xmax):
-    return (xi - xmin)/(xmax - xmin)
+testing_percentage = 0.20
+
+tdp_range = int(np.floor(testing_percentage * data_size / 2))
+testing_data_points = tdp_range * 2
 
 scaler = MinMaxScaler()
 
@@ -26,15 +29,17 @@ def encode(data):
             data[i,-1] = 0
     return data
 
-normalized_data = scaler.fit_transform(encode(stripped_data))
-pos_testing_data = normalized_data[-20:,:4]
-neg_testing_data = normalized_data[:20,:4]
-normalized_data = normalized_data[20:-20, :]
+encoded_stripped_data = encode(stripped_data)
+normalized_data = scaler.fit_transform(encoded_stripped_data)
+
+pos_testing_data = normalized_data[-tdp_range:,:4]
+neg_testing_data = normalized_data[:tdp_range,:4]
+normalized_data = normalized_data[tdp_range:-tdp_range, :]
+
 np.random.shuffle(normalized_data)
 
 results = normalized_data[:,4:]
 normalized_data = np.delete(normalized_data, 4, axis=1)
-
 
 
 def results_encode(data):
@@ -52,3 +57,4 @@ print(normalized_data)
 print(results)
 print(pos_testing_data)
 print(neg_testing_data)
+print(testing_data_points)
