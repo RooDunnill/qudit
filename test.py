d989ed105e497554b04a716763776058b6a0995c
Roo <dunnillt@gmail.com>
2025-07-21 15:10:52 +0100

    Created a classical machine learning algorithm

--- test.py
+++ test.py
@@ -1,5 +1,7 @@
 import numpy as np
 from sklearn.preprocessing import MinMaxScaler
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import accuracy_score
 from sklearn import svm
 
 data = []
@@ -7,11 +9,12 @@ data = np.loadtxt("diabetes_hackathon.csv", delimiter=",", dtype=str)
 data = data[1:, :]
 remove_cols = [2,3,5,6,7,8,9]
 stripped_data = np.delete(data, remove_cols, axis=1)
+features = 4
 
 data_size = len(data)
 
-testing_percentage = 0.20
-
+testing_percentage = 0.8
+training_percentage = 0.2
 tdp_range = int(np.floor(testing_percentage * data_size / 2))
 testing_data_points = tdp_range * 2
 
@@ -32,22 +35,23 @@ def encode(data):
 encoded_stripped_data = encode(stripped_data)
 normalized_data = scaler.fit_transform(encoded_stripped_data)
 
-pos_testing_data = normalized_data[-tdp_range:,:]
-neg_testing_data = normalized_data[:tdp_range,:]
-total_testing_data = np.append(pos_testing_data, neg_testing_data, axis=0)
+results = normalized_data[:,features:]
 
-normalized_data = normalized_data[tdp_range:-tdp_range, :]
+data = normalized_data[:,:features]
 
-np.random.shuffle(total_testing_data)
-np.random.shuffle(normalized_data)
+training_data, testing_data, training_results, testing_results = train_test_split(data, results, shuffle=True, test_size=testing_percentage, train_size=training_percentage)
 
-training_results = normalized_data[:,4:]
-testing_results = total_testing_data[:,4:]
+training_results = training_results.T[0]
+testing_results = testing_results.T[0]
 
-training_data = normalized_data[:,:4]
-testing_data = total_testing_data[:,:4]
-
-print(training_results)
-print(testing_results)
+print(f"Machine Learning Results:")
+print(f"Training Data size: {len(training_data)}")
+clf = svm.SVC(kernel = "linear")
 print(training_data)
 print(testing_data)
+print(training_results)
+print(testing_results)
+clf.fit(training_data, training_results)
+print(clf.predict(testing_data))
+print(clf.score(training_data, training_results))
+print(accuracy_score(testing_results, clf.predict(testing_data)))
